fb70a9515b78533389c7c5e669ba64c19f0170a8
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2015-11-15 13:29:49 +0100

    Changes in threshold alert functionality 	- now alert notification is displayed after checking the threshold values as set in the database (previously it was checking the hard-coded values)

--- IoT/platforms/android/assets/www/js/thresholdAlert.js
+++ IoT/platforms/android/assets/www/js/thresholdAlert.js
@@ -4,23 +4,23 @@ var forceZAlert = 0;
 
 function thresholdAlert(forceX, forceY, forceZ) {
 
-    if (forceX > 600) {
-            alert("inside check force X threshold");
+    if (forceX > thresholdX) {
+            //alert(thresholdX);
             forceXAlert = forceXAlert + 1;
             document.getElementById("xAlert").innerHTML = forceXAlert;
 
     }
 
-    if (forceY > 600) {
-            alert("inside check force Y threshold");
+    if (forceY > thresholdY) {
+            //alert("inside check force Y threshold");
 
             forceYAlert = forceYAlert + 1;
             document.getElementById("yAlert").innerHTML = forceYAlert;
 
     }
 
-    if (forceZ > 600) {
-        alert("inside check force Z threshold");
+    if (forceZ > thresholdZ) {
+        //alert("inside check force Z threshold");
         forceZAlert = forceZAlert + 1;
         document.getElementById("zAlert").innerHTML = forceZAlert;
 
