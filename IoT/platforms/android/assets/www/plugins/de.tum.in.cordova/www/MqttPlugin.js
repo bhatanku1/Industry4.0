a3b6b1e1b63880dd886c6b1211196bc4a33747f7
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2015-12-02 02:47:30 +0100

    Added comments in 	the Settings screen 	the Threshold screen 	the MQTT settings screen

--- IoT/platforms/android/assets/www/plugins/de.tum.in.cordova/www/MqttPlugin.js
+++ IoT/platforms/android/assets/www/plugins/de.tum.in.cordova/www/MqttPlugin.js
@@ -11,25 +11,19 @@ var forceZAlert= 0;
 
 
 MqttPlugin.prototype.setMQTT = function(aString){ console.log("MqttPlugin.js: showToast");
-    //alert('inside publish');
-    //var wf = 100;
-    //drawChart(wf);
     var t = aString.data + aString.topic;
     exec(
-    function(result){ /*alert("Mqttplugin publish got the value returned" + result); */},
-    function(result){ /*alert("Error" + reply);*/ },
+    function(result){ },
+    function(result){ },
     "MqttPlugin",
     "update",
     [aString.url, aString.port,aString.clientid,aString.username, aString.password]);
 };
 MqttPlugin.prototype.publish = function(aString){ console.log("MqttPlugin.js: showToast");
-    //alert('inside publish');
-    //var wf = 100;
-    //drawChart(wf);
     var t = aString.data + aString.topic;
     exec(
-    function(result){ /*alert("Mqttplugin publish got the value returned" + result); */},
-    function(result){ /*alert("Error" + reply);*/ },
+    function(result){ },
+    function(result){ },
     "MqttPlugin",
     "publish",
     [aString.topic, aString.data]);
@@ -37,72 +31,42 @@ MqttPlugin.prototype.publish = function(aString){ console.log("MqttPlugin.js: sh
 MqttPlugin.prototype.subscribe = function(aString){
 
     console.log("MqttPlugin.js: subscribe");
-    //alert('inside subscribe');
     exec(
-            function(result){   //alert("baalchaal");
-
-                                //document.getElementById("fHeader").innerHTML = "Values received from the device:",
-                                //alert("result[result]--->" + result['result']),
-                                i = i + 1,
+            function(result){   i = i + 1,
                                 res=result['result'].split(","),
-                                //alert("res[0]--->" + res[0]),
                                 forcex = res[0].split("="),
                                 forcey = res[1].split("="),
                                 forcez = res[2].split("="),
-                                //alert(res),
-                                //alert(forcex[1]),
-                                //alert(forcey[1]),
-                                //alert(parseInt(forcex),
-                                //alert(parseInt(forcey),
-                                //alert(parseInt(forcez),
                                 document.getElementById("forceX").innerHTML = "Force X: " + forcex[1] + " Nm",
                                 document.getElementById("forceY").innerHTML = "Force Y: " + forcey[1] + " Nm",
                                 document.getElementById("forceZ").innerHTML = "Force Z: " + forcez[1] + " Nm",
-                                //forcexformatted = replaceAll(forcex[1], "\0", ""),
                                 forcexformatted = forcex[1].split('\0').join(''),
                                 forceyformatted = forcey[1].split('\0').join(''),
                                 forcezformatted = forcez[1].split('\0').join(''),
-                                //alert("forceformat->" + forcexformatted),
                                 forceX = parseInt(forcexformatted),
                                 forceY = parseInt(forceyformatted),
                                 forceZ = parseInt(forcezformatted),
-                                //alert("forceX before graph call" + forceX),
                                 thresholdAlert(forceX, forceY, forceZ),
-
                                 graphUpdate(forceX, forceY, forceZ);
 
                         },
 
-    //function(result){ alert("Mqtt Sub got the value from the topic" + result['request.id']) },
-    function(result){ /*alert("Error" + reply);*/ },
-    "MqttPlugin",
-    "subscribe",
-    [aString.topic]);
-    //alert("kuchbhi");
-    //document.getElementById("test").innerHTML = "Mqtt Sub got the value from the topic";
+            function(result){ },
+            "MqttPlugin",
+            "subscribe",
+            [aString.topic]);
+
 };
 MqttPlugin.prototype.heartbeat = function(aString){
 
     console.log("MqttPlugin.js: heartbeat");
     exec(
-    function(result){ /*alert("Mqtt heartbeat got the value" + result["data"]),
-                     document.getElementById('connection_state').src = 'img/green.png',
-                     document.getElementById("status_ui").style.backgroundColor = "green",
-                     document.getElementById("pi_status").innerHTML = "Connection Successful",
-                     document.getElementById("wifi_ui_btn").disabled = false,
-                     document.getElementById("bluetooth_ui_btn").disabled = false,
-                     document.getElementById("opc_ui_btn").disabled = false
-                     document.getElementById("status_ui").style.backgroundColor = "black",
-                     document.getElementById("status_ui").style.color = "green",
-                     document.getElementById("pi_status").style.font = "bold",
-                     document.getElementById("start").disabled = false,
-                     */
-                     document.getElementById("pi_status").innerHTML = "Connection Successful",
-                     document.getElementById("start").getElementsByClassName("button")[0].removeAttribute("disabled"),
-                     document.getElementById("start").getElementsByClassName("button")[0].className="button button-clear button-balanced button-large ion-unlocked"
+    function(result){ document.getElementById("pi_status").innerHTML = "Connection Successful",
+                      document.getElementById("start").getElementsByClassName("button")[0].removeAttribute("disabled"),
+                      document.getElementById("start").getElementsByClassName("button")[0].className="button button-clear button-balanced button-large ion-unlocked"
                     },
 
-    function(result){ /*alert("Error" + reply);*/ },
+    function(result){ },
     "MqttPlugin",
     "heartbeat",
     [aString.topic]);
@@ -112,8 +76,8 @@ MqttPlugin.prototype.kill = function(aString){
 
     console.log("MqttPlugin.js: subscribe");
     exec(
-    function(result){ alert("Mqtt Subscribe got the value" + result) },
-    function(result){ /*alert("Error" + reply);*/ },
+    function(result){ },
+    function(result){ },
     "MqttPlugin",
     "kill",
     [aString.topic]);
